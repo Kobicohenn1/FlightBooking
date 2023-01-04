1596df93904e9521c89042d21339e6520be53750
kobicohen <kobicohenv1.1@gmail.com>
2023-01-04 12:16:36 +0200

    add attr to Both Classes

--- FlightBooking/FlightBooking/Models/Booking.cs
+++ FlightBooking/FlightBooking/Models/Booking.cs
@@ -1,6 +1,15 @@
-﻿namespace FlightBooking.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FlightBooking.Models
 {
     public class Booking
     {
+        [Key]
+        public int BookingId { get; set; }
+        public int DepartingFlightId { get; set; }
+        public int ReturnFlightId { get; set; } = -1;
+        public int DepartingSeatNumber { get; set; }
+
+
     }
 }
--- FlightBooking/FlightBooking/Models/Flight.cs
+++ FlightBooking/FlightBooking/Models/Flight.cs
@@ -1,6 +1,24 @@
-﻿namespace FlightBooking.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FlightBooking.Models
 {
     public class Flight
     {
+        [Key]
+        [Required]
+        public int FlightId { get; set; }
+        [Required]
+        public string OriginCountry { get; set; }
+        [Required]
+        public string DestinationCountry { get; set; }
+        [Required]
+        public DateTime FlightDate { get; set; }
+        [Required]
+        public TimeSpan ExpireDate { get; set; }
+        [Required]
+        public int Price { get; set; }
+        [Required]
+        public int AvailableSeats { get; set; }
+
     }
 }
